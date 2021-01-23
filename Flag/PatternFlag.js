1bb1d5dc2ac3911b2e3a77290a613641dbea509c
toxicboi <ffa123654789@gmail.com>
2021-01-23 14:20:01 +0800

    add love pattern

--- Flag/PatternFlag.js
+++ Flag/PatternFlag.js
@@ -2,19 +2,59 @@
 /** @type {HTMLCanvasElement} */ // 宣告作業環境
 
 // pattern1 = 圓形
-// pattern2 = 菱形
-// pattern3 = 六角形
-// pattern4 = 星型
+// pattern2 = 星星
+// pattern3 = 愛心
+// pattern4 = 六角形
 let patternconfig = {
    pattern1: function pattern1(Player) {
-      let size_in = Player.size_out * 13 / 21
       ctx.beginPath();
-      ctx.arc(Player.x, Player.y, size_in, 0, Math.PI * 2);
+      ctx.arc(Player.x, Player.y, Player.size_out * 13 / 22, 0, Math.PI * 2);
       ctx.stroke();
       ctx.closePath();
    },
    pattern2: function pattern2(Player) {
+      ctx.save();
 
+      ctx.translate(Player.x, Player.y)
+      ctx.lineWidth = Player.linewidth;
+      ctx.beginPath();
+      for (let i = 1; i <= 5; i++) {
+         ctx.lineTo(Math.cos((18 + 72 * i) / 180 * Math.PI) * (Player.size_out * 18 / 22), -Math.sin((18 + 72 * i) / 180 * Math.PI) * (Player.size_out * 18 / 22));
+         ctx.lineTo(Math.cos((54 + 72 * i) / 180 * Math.PI) * (Player.size_out * 18 / 22) / 2, -Math.sin((54 + 72 * i) / 180 * Math.PI) * (Player.size_out * 18 / 22) / 2);
+
+      }
+      ctx.closePath();
+      ctx.stroke();
+
+      ctx.restore();
+   },
+   pattern3: function pattern3(Player) {
+      ctx.save();
+
+      ctx.translate(Player.x,Player.y- 6 *scale)
+      ctx.moveTo(0, 0);
+      ctx.bezierCurveTo(0, -5, -5, -20, -25, -20);
+      ctx.bezierCurveTo(-55, -15, -63, 35, 0, 65);
+      ctx.bezierCurveTo(63, 35, 55, -15, 25, -20);
+      ctx.bezierCurveTo(5, -20, 0, -5, 0, -5);
+      ctx.stroke();
+
+      ctx.restore();
+   },
+   pattern4: function pattern4(Player) {
+      ctx.save();
+
+      ctx.translate(Player.x, Player.y)
+      ctx.lineWidth = Player.linewidth;
+      ctx.moveTo(0, 0);
+      ctx.beginPath();
+      for (let i = 1; i <= 6; i++) {
+         ctx.lineTo(Math.cos((60 * i) / 180 * Math.PI) * (Player.size_out * 18 / 22), -Math.sin((60 * i) / 180 * Math.PI) * (Player.size_out * 18 / 22));
+      }
+      ctx.closePath();
+      ctx.stroke();
+
+      ctx.restore();
    },
 
 }
