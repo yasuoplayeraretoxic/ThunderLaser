a35c846a738da36c21496f4da106b3baf1c037a5
toxicboi <ffa123654789@gmail.com>
2021-01-28 23:51:30 +0800

    small advise

--- Function/ButtonTouch.js
+++ Function/ButtonTouch.js
@@ -1,5 +1,6 @@
 function buttonTouch(player, buttonList) {
     //碰撞
+
     buttonList.forEach(e => {
         if (buttonColliDetect(player.x, player.y, e) === button.modeFlag) {
             return e.modeFlag
@@ -13,7 +14,6 @@ function buttonColliDetect(x, y, button) {
     let botlineleftright = (-(y - button.t3y) * (button.t3x - button.t4x) + (x - button.t3x) * (button.t3y - button.t4y) > 0 ? 1 : -1);
     let leftlineleftright = (-(y - button.t4y) * (button.t4x - button.t1x) + (x - button.t4x) * (button.t4y - button.t1y) > 0 ? 1 : -1);
     if (toplineleftright * botlineleftright === -1 && rightlineleftright * leftlineleftright === -1) {
-        console.log(modeFlag);
         return button.modeFlag
     }
     return 'nothing'
