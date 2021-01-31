b243b12ca7271d80fd86ac6b95848355104a7f34
toxicboi <ffa123654789@gmail.com>
2021-01-31 22:59:46 +0800

    intial pattern and color revise

--- Config.js
+++ Config.js
@@ -5,7 +5,7 @@ let laserLength = 90; //雷射長度
 let playerR = 22; //玩家大圓半徑
 
 let modeFlag = 'mode1'; //模式旗標
-let patternFlag = 'heart'; //玩家圖標參數
+let patternFlag = 'round'; //玩家圖標參數
 
 //紀錄器及開關
 let laserSwitch = false; //雷射開關
@@ -13,4 +13,4 @@ let laserList = []; //雷射陣列 儲存雷射
 let modeStack = ['mode9'] //模式旗標堆疊
 let time = 0; //時間紀錄器
 let scoreCount = 0; //分數紀錄器
-let nowColor = colorConfig['pink']; //顏色紀錄器
+let nowColor = colorConfig['blue']; //顏色紀錄器
